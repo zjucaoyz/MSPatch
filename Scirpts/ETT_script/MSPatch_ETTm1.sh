04b006bda5488cca938712cbaad3c983a760985f
Caoyz <67220479+zjucaoyz@users.noreply.github.com>
2024-08-09 23:12:58 +0800

    Create MSPatch_ETTm1.sh

---
+++ Scirpts/ETT_script/MSPatch_ETTm1.sh
@@ -0,0 +1,108 @@
+
+export CUDA_VISIBLE_DEVICES=0
+
+model_name=MSPatch
+
+seq_len=96
+e_layers=2
+learning_rate=0.01
+d_model=16
+d_ff=32
+train_epochs=10
+patience=10
+batch_size=16
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path  ./dataset/ETT-small/\
+  --data_path ETTm1.csv \
+  --model_id ETTm1_$seq_len'_'96 \
+  --model $model_name \
+  --data ETTh1 \
+  --features M \
+  --seq_len $seq_len \
+  --label_len 0 \
+  --pred_len 96 \
+  --e_layers $e_layers \
+  --enc_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 1 \
+  --d_model $d_model \
+  --d_ff $d_ff \
+  --learning_rate $learning_rate \
+  --train_epochs $train_epochs \
+  --patience $patience \
+
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTm1.csv \
+  --model_id ETTm1_$seq_len'_'192 \
+  --model $model_name \
+  --data ETTh1 \
+  --features M \
+  --seq_len $seq_len \
+  --label_len 48 \
+  --pred_len 192 \
+  --e_layers $e_layers \
+  --enc_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 1 \
+  --d_model $d_model \
+  --d_ff $d_ff \
+  --learning_rate $learning_rate \
+  --train_epochs $train_epochs \
+  --patience $patience \
+
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTm1.csv \
+  --model_id ETTm1_$seq_len'_'336 \
+  --model $model_name \
+  --data ETTh1 \
+  --features M \
+  --seq_len $seq_len \
+  --label_len 48 \
+  --pred_len 336 \
+  --e_layers $e_layers \
+  --enc_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 1 \
+  --d_model $d_model \
+  --d_ff $d_ff \
+  --learning_rate $learning_rate \
+  --train_epochs $train_epochs \
+  --patience $patience \
+
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTm1.csv \
+  --model_id ETTm1_$seq_len'_'720 \
+  --model $model_name \
+  --data ETTh1 \
+  --features M \
+  --seq_len $seq_len \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers $e_layers \
+  --enc_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 1 \
+  --d_model $d_model \
+  --d_ff $d_ff \
+  --learning_rate $learning_rate \
+  --train_epochs $train_epochs \
+  --patience $patience \
